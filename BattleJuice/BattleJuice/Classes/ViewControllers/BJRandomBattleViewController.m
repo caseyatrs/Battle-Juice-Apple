7ac3866a063f96897518d389410d42e680a158ec
Lance Parker <cymerc@gmail.com>
2012-04-04 19:58:05 -0600

    Basic UI setup completed for iPhone and iPad Rotation works based on device

--- BattleJuice/BattleJuice/Classes/ViewControllers/BJRandomBattleViewController.m
+++ BattleJuice/BattleJuice/Classes/ViewControllers/BJRandomBattleViewController.m
@@ -37,7 +37,11 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        return YES;
+    } else {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    }
 }
 
 @end
